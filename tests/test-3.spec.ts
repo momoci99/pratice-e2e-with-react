37ced488e9010c2d4de1cb3c087edebc9ae43e12
YeongKwonMo <momoci99@gmail.com>
2024-09-22 13:46:46 +0900

    thread holds

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from "@playwright/test";
 test("example test", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   await expect(page).toHaveScreenshot("example-test.png", {
-    maxDiffPixels: 50, // 100픽셀 차이 허용
+    threshold: 0.05, // 5% 차이까지 허용
   });
 });

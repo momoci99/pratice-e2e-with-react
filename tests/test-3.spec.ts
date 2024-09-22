0056f906cf60d5cfe28b60012196dae5c479a5de
YeongKwonMo <momoci99@gmail.com>
2024-09-22 14:31:23 +0900

    maxDiffPixelRatio 업데이트

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -4,6 +4,6 @@ test("example test", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   await expect(page).toHaveScreenshot("example-test.png", {
     fullPage: true,
-    maxDiffPixelRatio: 0.2,
+    maxDiffPixelRatio: 0.02,
   });
 });

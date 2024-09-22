d4bac12d180d3b9a39638526e25c0a272f55d48c
YeongKwonMo <momoci99@gmail.com>
2024-09-22 13:01:13 +0900

    설정 업데이트

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -2,5 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("example test", async ({ page }) => {
   await page.goto("http://localhost:5173/");
-  await expect(page).toHaveScreenshot("example-test.png");
+  await expect(page).toHaveScreenshot("example-test.png", {
+    maxDiffPixels: 50, // 100픽셀 차이 허용
+  });
 });

7456e1e853e686983a5ac7c7ddf82a317e23cae5
YeongKwonMo <momoci99@gmail.com>
2024-09-22 14:31:03 +0900

    ratio update

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from "@playwright/test";
 test("example test", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   await expect(page).toHaveScreenshot("example-test.png", {
-    threshold: 0.05, // 5% 차이까지 허용
+    fullPage: true,
+    maxDiffPixelRatio: 0.2,
   });
 });
